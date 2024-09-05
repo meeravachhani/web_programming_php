7dc6bea1ccd0f49fb4336e51d089114cbe168f81
meeravachhani <meeravachhani02@gmail.com>
2024-09-05 15:27:01 +0530

    Create script_6.php

---
+++ task/script_6.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "<b>good morning</b><br>";
+
+echo "<i>welcome to atmiya university</i><br>";
+
+echo "<h1>heading tag h1</h1>";
+
+echo "<h2>heading tag h2</h2>";
+
+echo "<h3>heading tag h3</h3>";
+
+echo "<h4>heading tag h4</h4>";
+
+echo "<h5>heading tag h5</h5>";
+
+echo "<h6>heading tag h6</h6>";
+
+
+?>
