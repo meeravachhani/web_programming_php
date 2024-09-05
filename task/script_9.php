a74aea2482f1216e806cba2932110bb04e598f6a
meeravachhani <meeravachhani02@gmail.com>
2024-09-05 15:30:34 +0530

    Create script_9.php

---
+++ task/script_9.php
@@ -0,0 +1,10 @@
+<?php
+$name="meera";
+$age="21";
+$city="rajkot";
+$mobile=1234567890;
+echo "my name is ".$name."<br>";
+echo "my age is ".$age."<br>";
+echo "my city is rajkot ".$city."<br>";
+echo "my mobile no is ".$mobile."<br>";
+?>
