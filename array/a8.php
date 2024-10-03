d914ea7f06410bfa3ab2299a286900f9df82c501
meeravachhani <meeravachhani02@gmail.com>
2024-10-03 10:16:18 +0530

    Create a8.php

---
+++ array/a8.php
@@ -0,0 +1,13 @@
+<?php
+$ceu = array(
+  "Italy" => "Rome",
+  "Luxembourg" => "Luxembourg",
+ "Belgium" => "Brussels",
+  "Denmark" => "Copenhagen",
+  "Finland" => "Helsinki",
+  "France" => "Paris",
+  
+);
+
+echo "The capital of Germany is " . $ceu["France"];
+?>
