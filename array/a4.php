721d70306d2e297ea0c76d89d7d896b73a60bffa
meeravachhani <meeravachhani02@gmail.com>
2024-10-03 10:14:29 +0530

    Create a4.php

---
+++ array/a4.php
@@ -0,0 +1,12 @@
+<?php
+$fruits1 = array("Orange", "Apple", "Banana", "Grapes", "Mango");
+
+sort($fruits1);
+echo "Fruits in Ascending Order: ";
+print_r($fruits1);
+
+echo "<br>";
+rsort($fruits1);
+echo "\nFruits in Descending Order: ";
+print_r($fruits1)
+?>
