a63443027c1f3122e54790c4b8ccd29005e4bc41
meeravachhani <meeravachhani02@gmail.com>
2024-10-03 10:13:37 +0530

    Create a2.php

---
+++ array/a2.php
@@ -0,0 +1,18 @@
+<?php
+
+$fruit = array("apple", "banana", "mango", "grapes","stoberi");
+
+print_r($fruit);
+
+echo "<br>";
+ 
+ $fruit[]="orange";
+ print_r($fruit);
+ echo "<br>";
+ $fruit[]="watermalen";
+ print_r($fruit);
+ echo "<br>";
+
+ $fruit[0]="chery";
+ print_r($fruit);
+?>
