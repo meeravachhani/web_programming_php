2b8614efdc09314d97418aa58a5b54f245d6eaf4
meeravachhani <meeravachhani02@gmail.com>
2024-10-03 10:15:23 +0530

    Create a6.php

---
+++ array/a6.php
@@ -0,0 +1,18 @@
+<?php
+
+$countries = array(
+    "France" => "Paris","Japan" => "Tokyo","Australia" => "Canberra","Brazil" => "Brasilia",
+    "China" => "Beijing"
+);
+
+echo "Country-Capital Pairs:";
+echo "<br>";
+foreach ($countries as $country => $capital) {
+    echo "$country: $capital<br>";
+}
+
+?>
+
+
+
+
