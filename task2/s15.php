3c26658c2224c6b02977208c3d01166c9247746b
meeravachhani <meeravachhani02@gmail.com>
2024-11-27 19:37:35 +0530

    Create s15.php

---
+++ task2/s15.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="script_15.php" method="post">
+        enter first number:
+        <input type="text" name="fir">
+        enter second number:
+        <input type="text" name="sec">
+        enter third number:
+        <input type="text" name="thi">
+        <input type="submit">
+    </form>
+</body>
+</html>
