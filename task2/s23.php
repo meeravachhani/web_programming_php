0b709cdc2d282b285a65d38bc1b16ce5a474a627
meeravachhani <meeravachhani02@gmail.com>
2024-11-27 20:05:11 +0530

    Create s23.php

---
+++ task2/s23.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form  action="script_23.php" method="post">
+            Enter number 1 <input type="text" name="num1"><br>
+            Enter number 2 <input type="text" name="num2"><br>
+            Enter number 3 <input type="text" name="num3">
+            <br><input type="submit">
+        </form>
+</body>
+</html>
