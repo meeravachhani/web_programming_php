8add38e5f7f531c05893bd39716b58a1e550408f
meeravachhani <meeravachhani02@gmail.com>
2024-11-27 19:39:02 +0530

    Create s18.php

---
+++ task2/s18.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form method="post" action="script_18.php">
+    Guess a number between 1 and 10: <input type="number" name="guess" min="1" max="10"><br><br>
+    <input type="submit" name="submit" value="Submit Guess">
+</form>
+</body>
+</html>
