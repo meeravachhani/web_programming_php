7fe5b5d8b5c9782f1c9860ac148a8211121258d9
meeravachhani <meeravachhani02@gmail.com>
2024-11-27 19:36:45 +0530

    Create s12.php

---
+++ task2/s12.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="script_12.php" method="post">
+    enter your first string:
+    <input type="text" name="string1">
+    enter your second string:
+    <input type="text" name="string2">
+    <input type="submit">
+    </form>
+</body>
+</html>
