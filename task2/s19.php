ff609ec466929ef56461b8876fb1266fb2bc74b3
meeravachhani <meeravachhani02@gmail.com>
2024-11-27 19:39:22 +0530

    Create s19.php

---
+++ task2/s19.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form method="post" action="script_19.php">
+    Enter a positive integer: <input type="number" name="number" min="1"><br><br>
+    <input type="submit" name="submit" value="Check Prime">
+</form>
+</body>
+</html>
