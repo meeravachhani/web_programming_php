e3bf962b86a27651eb8cb56c6dc66877c22adba5
meeravachhani <meeravachhani02@gmail.com>
2024-11-27 20:04:49 +0530

    Create s22.php

---
+++ task2/s22.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="script_22.php" method="post">
+Enter radious of circle
+<input type="text" name="circle">
+<input type="submit">
+</form>
+</body>
+</html>
